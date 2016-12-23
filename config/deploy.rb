049f4d680e71019554311d1431335bc631e6a467
Unayung Chen <unayung@gmail.com>
2016-12-23 18:29:22 +0800

    add csv folder share

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/config.yml', 'config/schedule.rb')
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'public/csv_files')
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
